3ede521d433e8215c60a457b4b6783775eb43363
Dima <dimastarina@gmail.com>
2024-04-20 12:34:22 +0300

    added pino pretty

--- src/logger.js
+++ src/logger.js
@@ -1,3 +1,24 @@
 import pino from 'pino';
 
-export default pino({});
+const transport = pino.transport({
+    targets: [
+        // stdout
+        {
+            target: 'pino-pretty',
+            options: {
+                colorize: true,
+                destination: 1,
+            },
+        },
+        // file
+        {
+            target: 'pino-pretty',
+            options: {
+                colorize: false,
+                destination: './bbot.log',
+            },
+        },
+    ],
+});
+
+export default pino(transport);
